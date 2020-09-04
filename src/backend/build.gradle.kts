b969a294ff1d179eb5052dc0b9a686192a283f65
Jéf <buenojeferson@outlook.com>
2020-09-03 21:56:10 -0300

    🗃 Alteração do banco de dados para SQL Server

--- src/backend/build.gradle.kts
+++ src/backend/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("io.springfox:springfox-swagger2:2.7.0")
 	implementation("io.springfox:springfox-swagger-ui:2.7.0")
-	implementation("org.postgresql:postgresql")
+	implementation("com.microsoft.sqlserver:mssql-jdbc")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
