1771d9bd29cce80068ef665514d76cfeba3a904b
Jéf <buenojeferson@outlook.com>
2020-09-09 18:42:36 -0300

    🗃 Alteração da base de dev para H2

--- src/backend/build.gradle.kts
+++ src/backend/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("io.springfox:springfox-swagger2:2.7.0")
 	implementation("io.springfox:springfox-swagger-ui:2.7.0")
 	implementation("com.microsoft.sqlserver:mssql-jdbc")
+	implementation("com.h2database:h2:1.4.196")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
--- src/backend/src/main/kotlin/dev3/estouropilha/rastreio/backend/model/Pessoa.kt
+++ src/backend/src/main/kotlin/dev3/estouropilha/rastreio/backend/model/Pessoa.kt
@@ -1,11 +1,13 @@
 package dev3.estouropilha.rastreio.backend.model
 
+import javax.persistence.Table
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
+@Table(name = "Pessoas")
 class Pessoa(
         val nome: String,
         @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Int = -1
